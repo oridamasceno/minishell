4fd23bea422b1abb38b7b8d11823fb977a738692
Larissa <larissa.jordana15@gmail.com>
2024-09-03 21:46:16 -0300

    doing_agora, a função separa por palavras e frases dentro de aspas. :)

--- funcao_que_separa_input.c
+++ funcao_que_separa_input.c
@@ -1,18 +1,33 @@
 /*
  * Compilação: "cc -Wall -Wextra -Werror funcao_que_separa_input.c -lreadline". !!!
  *
- * 39 - aspas simples = 1;
- * 34 - aspas duplas = 2;
+ * 39 - aspas simples;
+ * 34 - aspas duplas;
+ * 124 - pipe;
 */
 
 #include "includes.h"
 #include "minishell.h"
 
-int	essa_aspa_tem_fechamentoP(char *str, int idx)
+int tem_espaco(int c)
 {
-	int	tipo = str[idx];
+	if (c == ' ' || c == '\t')
+		return (c);
+	return (0);
+}
+
+int	essa_aspa_tem_fechamentoP(char *str, int idx, int tipo)
+{
+
 	while (str[++idx])
 	{
+		if (tipo == 1)
+		{
+			if (tem_espaco(str[idx]))
+				return (idx);
+			else if(!str[idx + 1])
+				return (++idx);
+		}
 		if (str[idx] == tipo)
 			return (idx);
 	}
@@ -26,10 +41,10 @@ char	*frase_dentro_das_aspas(char *str, int idx)
 	int	fechamento;
 	char	*retorno;
 	
-	fechamento = essa_aspa_tem_fechamentoP(str, idx);
+	fechamento = essa_aspa_tem_fechamentoP(str, idx, str[idx]);
 	if (!fechamento)
 		return (NULL);
-	len = (fechamento - idx);
+	len = fechamento - idx;
 	retorno = (char *) malloc(len * sizeof(char));
 	if (!retorno)
 		return (NULL);
@@ -40,11 +55,67 @@ char	*frase_dentro_das_aspas(char *str, int idx)
 	return (retorno);
 }
 
+char	*palavra_unica(char *str, int idx)
+{
+	int len;
+	int ultimo;
+	int iret;
+	char *ret;
+
+	ultimo = essa_aspa_tem_fechamentoP(str, idx, 1);
+	if (!ultimo)
+		return (NULL);
+	len = (ultimo - idx) + 1;
+	ret = (char *) malloc(len * sizeof(char));
+	iret = -1;
+	while (idx < ultimo)
+		ret[++iret] = str[idx++];
+	ret[iret + 1] = '\0';
+	return (ret);
+}
+
+/*----------------------------------------------------------------------*/
+
+int input_tokens(char *str)
+{
+	int	i = 0;
+	int count = 0;
+	char *unico;
+
+	while(tem_espaco(str[i]))
+		i++;
+	while(str[i])
+	{
+		if(str[i] == 34 || str[i] == 39)
+		{
+			unico = frase_dentro_das_aspas(str, i);
+			if (!unico)
+				return (1);
+			printf("%d - Achei aspas		|%s|\n", count++, unico);
+			free(unico);
+			i = essa_aspa_tem_fechamentoP(str, i, str[i]);
+		}
+		else if (!tem_espaco(str[i]))
+		{
+			unico = palavra_unica(str, i);
+			if (!unico)
+				return (2);
+			printf("%d - Achei uma palavra	|%s|\n", count++, unico);
+			free(unico);
+			i = essa_aspa_tem_fechamentoP(str, i, 1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/*-----------------------------------------------------------------------*/
+
 int main(void)
 {
 	char	*input;
 	char	*aspas;
-	int	i;
+	int	ret_itokens;
 
 	printf("Bem-vindo ao nosso start!\n");
 	printf("Digite 'sair' para sair.\n");
@@ -60,26 +131,18 @@ int main(void)
 			break ;
 		}
 
-		i = 0;
-		while (input[i])
+		ret_itokens = input_tokens(input);
+		if (ret_itokens == 1)
 		{
-			if (input[i] == 34 || input[i] == 39)
-			{
-				aspas = frase_dentro_das_aspas(input, i);
-				if (!aspas)
-				{
-					rl_on_new_line();
-					rl_replace_line("Aspas sem fechamento.\n", 0);
-					rl_redisplay();
-				}
-				else
-				{
-					printf("%s|\n", aspas);
-					free(aspas);
-					i = essa_aspa_tem_fechamentoP(input, i);
-				}
-			}
-			i++;
+			rl_on_new_line();
+			rl_replace_line("Aspas sem fechamento.\n", 0);
+			rl_redisplay();
+		}
+		else if (ret_itokens == 2)
+		{
+			rl_on_new_line();
+			rl_replace_line("Erro na alocação de palavras.\n", 0);
+			rl_redisplay();
 		}
 		free(input);
 	}
--- minishell.h
+++ minishell.h
@@ -1,19 +1,13 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define MAX_SIZE 1024
 
 # include "includes.h"
 # include "./libft/libft.h"
 
 typedef struct s_token {
-	char	*str_token;
+	char	str_token[MAX_SIZE];
 	struct s_token *next;
 }		t_token;
-
-typedef struct s_quotes {
-	int	position;
-	int	type_token;
-	char	*str_quote;
-	struct s_quotes *next;
-}		t_quotes;
 
 #endif
